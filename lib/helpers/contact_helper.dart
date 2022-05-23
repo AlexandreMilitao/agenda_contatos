ddd15c515afb161e82ac7e98dcdfccca32c05b34
AleLevi <ale.levi@hotmail.com>
2022-05-23 11:53:09 -0300

    Database_commit

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,1 +1,76 @@
-class ContactHelper {}
+import 'dart:html';
+import 'package:path/path.dart';
+import "dart:async";
+import 'package:sqflite/sqflite.dart';
+
+final String contactTable = "contactTable";
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emaiColumn";
+final String phoneColumn = "phoneColumns";
+final String imgColumn = "imgColumn";
+
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  late Database _db;
+
+  Future<Database> get db async {
+    if (db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contact.db");
+
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerversion) async {
+      await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY,$nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)");
+    });
+  }
+}
+
+class Contact {
+  late int id;
+  late String name;
+  late String email;
+  late String phone;
+  late String img;
+
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: img
+    };
+
+    if (id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)";
+  }
+}
